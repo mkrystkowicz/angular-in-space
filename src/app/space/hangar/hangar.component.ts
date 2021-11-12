d05fd4e64a59cd75e7540b3e6291909759047670
Mateusz Krystkowicz <krissie515@gmail.com>
2021-11-12 22:17:41 +0100

    Misja 2

---
+++ src/app/space/hangar/hangar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hangar',
+  templateUrl: './hangar.component.html',
+  styleUrls: ['./hangar.component.css'],
+})
+export class HangarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
